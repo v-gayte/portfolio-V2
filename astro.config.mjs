fa405feba73954baad7d9cac4b4ed54bc32402fb
kat00ch <virgil.gayte@gmail.com>
2026-03-11 21:13:13 +0100

    maj

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,8 @@
 // @ts-check
 import { defineConfig } from "astro/config";
-
 import tailwindcss from "@tailwindcss/vite";
-import vercel from "@astrojs/vercel";
+import node from "@astrojs/node"; // Importation de l'adaptateur Node
 import mdx from "@astrojs/mdx";
-
 import react from "@astrojs/react";
 
 // https://astro.build/config
@@ -13,7 +11,11 @@ export default defineConfig({
     plugins: [tailwindcss()],
   },
 
-  adapter: vercel(),
+  output: 'server', // Active le mode Server-Side Rendering (SSR)
+  adapter: node({
+    mode: "standalone", // Génère un serveur prêt à l'emploi
+  }),
+
   integrations: [mdx(), react()],
 
   i18n: {
@@ -24,4 +26,4 @@ export default defineConfig({
       redirectToDefaultLocale: false,
     },
   },
-});
+}); 
